7c17a34c5ce7822940a007f1a2d98dfdfc44d0a6
jdhnxd1-cyber <jdhnxd1@gmail.com>
2026-08-07 15:29:57 +0300

    Update analyze.js

--- analyze.js
+++ analyze.js
@@ -8,8 +8,7 @@ export default async function handler(req, res) {
   try {
     const { prompt } = req.body;
     const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
-   const model = genAI.getGenerativeModel({ model: "gemini-2.0-flash" });
-
+  const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
     const result = await model.generateContent(prompt);
     const response = await result.response;
     const text = response.text();
